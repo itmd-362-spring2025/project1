ec8aa4e8a5a3a809fda7c45fca6af68d0fda0657
rduong11 <raymondduong12@gmail.com>
2025-03-03 23:06:08 -0600

    code cleanup + js broke...

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const demoButton = document.querySelector("input[value='Demo']");
+  console.log("Script loaded");
+  const demoButton = document.querySelector("demoButton");
+  console.log(demoButton);
   const displayCoordinates = document.getElementById("coordinates");
 
   demoButton.addEventListener("click", () => {
+    console.log("button clicked")
     displayCoordinates.textContent = "Coordinates: 41.838506, -87.627907";
   })
 });
