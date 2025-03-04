dbef62ab6ca84955d93bcddb6732e79bc89a630f
rduong11 <raymondduong12@gmail.com>
2025-03-03 22:44:02 -0600

    debugging msg

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const displayCoordinates = document.getElementById("coordinates");
 
   demoButton.addEventListener("click", () => {
+    console.log("button clicked");
     displayCoordinates.textContent = "Coordinates: 41.838506, -87.627907";
   })
 });
