4c798003f1dda738da2224d86682978ecfb6dabe
rduong11 <raymondduong12@gmail.com>
2025-03-04 18:19:54 -0600

    changed to defer and cleaned up debugging code

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Script loaded");
   const demoButton = document.querySelector("#demoButton");
-  console.log(demoButton);
   const displayCoordinates = document.getElementById("coordinates");
 
   demoButton.addEventListener("click", () => {
-    console.log("button clicked")
     displayCoordinates.textContent = "Coordinates: 41.838506, -87.627907";
   })
 });
